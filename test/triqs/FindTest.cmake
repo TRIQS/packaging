17dd7dcc599aff26c6bf4844758773f2ca0f13f3
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-08-31 14:12:19 -0400

    [cmake] Fix triqs testing environment for mpi tests to suppress warnings

--- test/triqs/FindTest.cmake
+++ test/triqs/FindTest.cmake
@@ -8,6 +8,7 @@ find_package(TriqsTest)
 #   where my_code is the cpp executable my_code.ref is the expected output
 macro(add_cpp_test testname)
   triqs_add_cpp_test(${testname})
+  set_property(TEST ${testname_} APPEND PROPERTY ENVIRONMENT OMPI_MCA_btl_base_warn_component_unused=0)
 endmacro(add_cpp_test)
 
 # runs a python test
